1098184c257dad1080c918cb436273da649d1af3
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-02-22 12:58:49 -0500

    add error handling

--- API_Files/NLP.py
+++ API_Files/NLP.py
@@ -3,4 +3,6 @@ from flask_restful import Api, Resource
 
 class NLP(Resource):
     def get(self):
+        if(data.isempty()):
+            return("ERROR: 400-level (Client error) client sent an invalid request")
         return {"data": "Natural Language is processed!"}
